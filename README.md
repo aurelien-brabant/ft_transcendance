98caa85996cac8cc13846685d503269b466497aa
abrabant <abrabant@student.42.fr>
2022-01-19 13:29:38 +0100

    Add docker-compose for development

---
+++ README.md
@@ -0,0 +1,3 @@
+# ft_transcendance
+
+A frankly over-engineered but not less cool web implementation of the Pong game.
---
+++ nestjs-back/src/app.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): { msg: string } {
+  	return {
+		msg: 'Hello world from NestJS!'
+	};
+  }
+}
