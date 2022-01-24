ccc16384ead8b708e8a4248fe8ea7ec14678e519
aldubar <aldubar@student.42.fr>
2022-01-25 00:10:55 +0100

    basic 42 OAuth using API to login... need to improve this feature...

---
+++ src/nextjs-front/pages/dashboard.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/Home.module.css'
+
+const Auth: NextPage = () => {
+    return (
+      <div className={styles.container}>
+      <h1>Dashboard</h1>
+      
+      <footer className={styles.footer}>
+        <a href="/" rel="noopener noreferrer">
+            Back to Home
+        </a>
+      </footer>
+      </div>
+    )
+  }
+  
+  export default Auth
