3405c5f889a79d19b8ca9662e5a05bf1b23b3684
abrabant <abrabant@student.42.fr>
2022-01-27 21:23:07 +0100

    Fix navlinks

--- src/nextjs-front/constants/nav.ts
+++ src/nextjs-front/constants/nav.ts
@@ -8,16 +8,16 @@ export const wildNavItems: NavItem[] = [
 	{
 		id: 'discover',
 		label: 'Discover',
-		href: '/home#discover',
+		href: '/#discover',
 	},
 	{
 		id: 'features',
 		label: 'Features',
-		href: '/home#features',
+		href: '/#features',
 	},
 	{
 		id: 'team',
 		label: 'The team',
-		href: '/home#team',
+		href: '/#team',
 	}
 ];
