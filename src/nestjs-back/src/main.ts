ee7a0675983f8c393e68b6c261780a20dd5b204b
abrabant <abrabant@student.42.fr>
2022-01-29 02:30:57 +0100

    Add NestJS swagger integration

--- src/nestjs-back/src/main.ts
+++ src/nestjs-back/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import {NestExpressApplication} from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  console.log(__dirname);
+
+  if (process.env.NODE_ENV !== 'development') {
+    const customOptions: SwaggerCustomOptions = {
+      customCssUrl: '/material_swagger.css'
+    }
+
+    const config = new DocumentBuilder()
+      .setTitle('ft_transcendance')
+      .setDescription(
+        'The API that supports ft_transcendance, a wonderful web-based multiplayer pong game',
+      )
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('apidoc', app, document, customOptions);
+  }
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   await app.listen(3000);
 }
+
 bootstrap();
