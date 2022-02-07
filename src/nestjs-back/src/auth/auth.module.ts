11eeb70bade806d006070b8b849b3d2883391d9f
abrabant <abrabant@student.42.fr>
2022-02-07 05:22:27 +0100

    Add JWT emission and associated NestJS Guard

--- src/nestjs-back/src/auth/auth.module.ts
+++ src/nestjs-back/src/auth/auth.module.ts
@@ -4,10 +4,16 @@ import {UsersModule} from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import {LocalStrategy} from './local.strategy';
 import { AuthController } from './auth.controller';
+import {JwtModule} from '@nestjs/jwt';
+import {JwtStrategy} from './jwt.strategy';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [UsersModule, PassportModule, JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_LIFETIME || '15m' } // use JWT_LIFETIME and fallback to 15 minutes if not set
+    })
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
--- src/nestjs-back/src/auth/auth.service.ts
+++ src/nestjs-back/src/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import {UsersService} from 'src/users/users.service';
 import { compare as comparePassword } from 'bcrypt';
+import {Users} from 'src/users/entities/users.entity';
+import {JwtService} from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersServices: UsersService) {}
+  constructor(private usersServices: UsersService, private readonly jwtService: JwtService) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersServices.findOneByEmail(email);
@@ -15,6 +17,13 @@ export class AuthService {
     }
 
     return null;
-
   };
+
+  async generateJWT(user: Users) {
+    const payload = { sub: user.id };
+
+    return {
+      access_token: this.jwtService.sign(payload)
+    };
+  }
 }
