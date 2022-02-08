8d034d6bcc8db027612b6c4f8cc7d1b7573ed243
abrabant <abrabant@student.42.fr>
2022-02-08 18:23:18 +0100

    Add proper design for authorization page

--- src/nextjs-front/pages/validate-fortytwo.tsx
+++ src/nextjs-front/pages/validate-fortytwo.tsx
@@ -1,5 +1,11 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import HashLoader from "react-spinners/HashLoader";
+import { BiSad } from "react-icons/bi";
+import Image from "next/image";
+import withWildLayout from "../components/hoc/withWildLayout";
+
+import { authorizationLink } from "../constants/authorize42";
 
 const ValidateFortyTwo = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -12,11 +18,11 @@ const ValidateFortyTwo = () => {
     const requestURI = `http://localhost/api/auth/login42`;
 
     fetch(requestURI, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ apiCode: searchParams.get('code')})
+      body: JSON.stringify({ apiCode: searchParams.get("code") }),
     }).then((res) => {
       if (res.status === 201) {
         res.json().then(({ access_token }) => {
@@ -25,17 +31,64 @@ const ValidateFortyTwo = () => {
         });
       } else {
         setError("An error occured");
+        setIsLoading(false);
       }
-
-      setIsLoading(false);
     });
   }, []);
 
-  if (isLoading) {
-    return <h1> Validating forty two... </h1>;
-  }
-
-  return <h1 className="text-white bg-red-600">{error}</h1>;
+  return (
+    <div
+      className="relative flex flex-col items-center justify-center min-h-screen bg-fixed bg-gray-900 bg-center bg-no-repeat bg-cover gap-y-4"
+      style={{
+        backgroundImage: `url('/triangles.png')`,
+      }}
+    >
+      <main className="container flex flex-col items-center mx-auto gap-y-16">
+        {isLoading ? (
+          <Fragment>
+            <HashLoader color="#db2777" size={200} />
+            <div className="flex flex-col text-center gap-y-2">
+              <h1 className="text-3xl font-bold text-white left-4 top-4">
+                We are logging you in using your 42 account. You will be
+                redirected in a few seconds.
+              </h1>
+              <small className="text-xl text-neutral-200">
+                Not redirected after a few minutes? Try to{" "}
+                <a
+                  href={authorizationLink}
+                  className="underline underline-offset-4 hover:text-pink-600"
+                >
+                  restart the authorization process
+                </a>
+                .
+              </small>
+            </div>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <Image src={"/this_is_fine.gif"} width={498} height={280} />
+            <div className="flex flex-col text-center gap-y-2">
+              <h1 className="text-3xl font-bold text-white text-red-600 left-4 top-4">
+                An error occured during the authorization process.
+              </h1>
+              <small className="text-xl leading-normal text-left text-neutral-200">
+                It may be because your authorization code has already been used
+                or has expired. <br /> You can try to{" "}
+                <a
+                  href={authorizationLink}
+                  className="underline underline-offset-4 hover:text-pink-600"
+                >
+                  restart the authorization process
+                </a>
+                .<br />
+                If the problem persists, feel free to contact us.
+              </small>
+            </div>
+          </Fragment>
+        )}
+      </main>
+    </div>
+  );
 };
 
-export default ValidateFortyTwo;
+export default withWildLayout(ValidateFortyTwo);
