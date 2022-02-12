9cbd7f7a7929728150daa4b34b228061cb9caaed
ouram <mboivin@student.42.fr>
2022-02-12 13:01:20 +0100

    Add skeleton files

---
+++ src/nestjs-back/src/channels/channels.controller.ts
@@ -0,0 +1,15 @@
+import { query, Request } from 'express';
+import { ChannelsService } from './channelss.service';
+
+@Controller('api/chat')
+export class ChannelsController {
+    constructor() {}
+
+    // @Get()
+
+    // @Post()
+
+    // @Patch(':id')
+
+    // @Delete(':id')
+}
