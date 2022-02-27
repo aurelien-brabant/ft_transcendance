c97d83eb891d32cfcc8167e21b7f17e2e7592c37
ouram <mboivin@student.42.fr>
2022-02-27 17:11:54 +0100

    Update Users dto

--- src/nestjs-back/src/users/dto/create-user.dto.ts
+++ src/nestjs-back/src/users/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
-import { IsString, IsEmail, IsOptional, IsInt, IsPhoneNumber } from 'class-validator';
+import {
+  IsEmail,
+  IsInt,
+  IsOptional,
+  IsString
+} from 'class-validator';
+import { Channels } from 'src/channels/entities/channels.entity';
 import { Games } from 'src/games/entities/games.entity';
+import { Messages } from 'src/messages/entities/messages.entity';
 import { Users } from '../entities/users.entity';
 
 export class CreateUserDto {
@@ -14,9 +21,25 @@ export class CreateUserDto {
     //    @IsInt({ each: true })
       //  readonly games: number[];
     readonly games: Games[];
-    
+
+    @IsOptional()
+    readonly wins: Games[];
+
+    @IsOptional()
+    @IsInt()
+    readonly losses: number;
+
     @IsOptional()
     //    @IsInt({ each: true })
     //    readonly friends: number [];
     readonly friends: Users[];
+
+    @IsOptional()
+    readonly ownedChannels: Channels[];
+
+    @IsOptional()
+    readonly joinedChannels: Channels[];
+
+    @IsOptional()
+    readonly sentMessages: Messages[];
 }
--- src/nestjs-back/src/users/dto/update-user.dto.ts
+++ src/nestjs-back/src/users/dto/update-user.dto.ts
@@ -1,10 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsInt,
+  IsOptional,
+  IsPhoneNumber,
+  IsString
+} from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
-import { IsInt, IsOptional, IsPhoneNumber, IsString } from 'class-validator';
-import { Channels } from 'src/channels/entities/channels.entity';
-import { Games } from 'src/games/entities/games.entity';
-import { Messages } from 'src/messages/entities/messages.entity';
-import { Users } from '../entities/users.entity';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
     @IsOptional()
@@ -26,28 +27,4 @@ export class UpdateUserDto extends PartialType(CreateUserDto) {
     @IsOptional()
     @IsInt()
     readonly rank: number;
-
-    @IsOptional()
-//    @IsInt({ each: true })
-  //  readonly games: number[];
-    readonly games: Games [];
-
-    @IsOptional()
-    readonly wins: Games[];
-
-    @IsOptional()
-    @IsInt()
-    readonly losses: number;
-
-    @IsOptional()
-    readonly friends: Users[];
-
-    @IsOptional()
-    readonly ownedChannels: Channels[];
-
-    @IsOptional()
-    readonly joinedChannels: Channels[];
-
-    @IsOptional()
-    readonly sentMessages: Messages[];
 }
