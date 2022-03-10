807173a02797bf8a220544d9775cf300df6c0f08
ouram <mboivin@student.42.fr>
2022-03-10 16:40:18 +0100

    Start ChatGateway

---
+++ src/nestjs-back/src/chat/chat.gateway.ts
@@ -0,0 +1,49 @@
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+
+@WebSocketGateway({ cors: true })
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('ChatGateway');
+
+  afterInit(server: Server) {
+    this.logger.log('Init ChatGateway');
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    this.logger.log(`Received message: ${message}`);
+    this.server.emit('message', message);
+  }
+
+  @SubscribeMessage('joinChannel')
+  handleJoinChannel(client: Socket, channelName: string) {
+    client.join(channelName);
+    this.logger.log(`Client [${client.id}] joined channel ${channelName}`);
+    client.emit('joinedChannel', channelName);
+  }
+
+  @SubscribeMessage('leaveChannel')
+  handleLeftChannel(client: Socket, channelName: string) {
+    client.leave(channelName);
+    this.logger.log(`Client [${client.id}] left channel ${channelName}`);
+    client.emit('leftChannel', channelName);
+  }
+}
--- src/nestjs-back/src/gateway/gateway.module.ts
+++ src/nestjs-back/src/gateway/gateway.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { PongGateway } from './pong.gateway'
+import { PongGateway } from './pong.gateway';
+import { ChatGateway } from '../chat/chat.gateway';
 
-@Module({ providers: [PongGateway] })
+@Module({ providers: [PongGateway, ChatGateway] })
 export class GatewayModule {}
