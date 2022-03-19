37d2d52f3ce3390fdda70824e5adef258538c0fa
aldubar <aldubar@student.42.fr>
2022-03-19 18:46:20 +0100

    finish css

--- src/nextjs-front/pages/friends.tsx
+++ src/nextjs-front/pages/friends.tsx
@@ -5,31 +5,36 @@ import ResponsiveFade from "../components/ResponsiveFade";
 import Image from 'next/image';
 import { useContext, useEffect, useState } from "react";
 import { BounceLoader } from "react-spinners";
-import { FaUserFriends, FaUserSlash, FaUsersSlash } from "react-icons/fa";
+import { FaMedal, FaUserFriends, FaUserSlash, FaUsersSlash } from "react-icons/fa";
 import authContext, { AuthContextType } from "../context/auth/authContext";
 import { UserStatusItem } from "../components/UserStatus";
 import Tooltip from "../components/Tooltip";
 import { RiUserHeartLine, RiUserSettingsLine } from "react-icons/ri";
 import { useRouter } from "next/router";
-import { AiOutlineUserAdd, AiOutlineUserDelete } from "react-icons/ai";
+import { AiOutlineFall, AiOutlineUserAdd, AiOutlineUserDelete } from "react-icons/ai";
 import Link from "next/link";
 import alertContext, { AlertContextType } from "../context/alert/alertContext";
+import { MdCancel } from "react-icons/md";
+import { GiFalling, GiPodiumWinner } from "react-icons/gi";
 
 export type List = {
   id: string;
   username: string;
+  duoquadra_login: boolean;
+  wins: number;
+  losses: number;
+ // rank: number
 };
 
-const Table: React.FC<{ type: string, list: List[], setList: any, suggested: List[], setSuggested: any, friendsLen: number, setFriendsLen: any, suggestedLen: number, setSuggestedLen: any, blockedLen: number, setBlockedLen: any, blockedUsers: List[], setBlockedUsers: any, friends: List[], setFriends: any }> = ({
-  type, list, setList, suggested, setSuggested, friendsLen, setFriendsLen, suggestedLen, setSuggestedLen, blockedLen, setBlockedLen, blockedUsers, setBlockedUsers, friends, setFriends
+const Table: React.FC<{ type: string, list: List[], suggested: List[], setSuggested: any, friendsLen: number, setFriendsLen: any, blockedLen: number, setBlockedLen: any, blockedUsers: List[], setBlockedUsers: any, friends: List[], setFriends: any, friends42Len: number, setFriends42Len: any }> = ({
+  type, list, suggested, setSuggested, friendsLen, setFriendsLen, blockedLen, setBlockedLen, blockedUsers, setBlockedUsers, friends, setFriends, friends42Len, setFriends42Len
 }) => {
 
   const { setAlert } = useContext(alertContext) as AlertContextType;
   const { getUserData } = useContext(authContext) as AuthContextType;
   
   //TODO transofmr adding to requesting 
-  //TODO update friends42,list
-  const requestFriend = async (id: string, username: string) => {
+  const requestFriend = async (id: string, username: string, isDuoQuadra: boolean) => {
     const req = await fetch (`/api/users/${getUserData().id}`, {
       method: "PATCH",
       headers: {
@@ -50,8 +55,8 @@ const Table: React.FC<{ type: string, list: List[], setList: any, suggested: Lis
         item => item.id !== id
       ));
       setFriendsLen(friendsLen + 1);
+      isDuoQuadra && setFriends42Len(friends42Len + 1);
       setAlert({ type: 'info', content: `Friend request sent to ${username}` });
-     // router.reload();
     }
     else
       setAlert({ type: 'error', content: `Error while sending friend request to ${username}` });
@@ -103,27 +108,35 @@ const Table: React.FC<{ type: string, list: List[], setList: any, suggested: Lis
       setAlert({ type: 'error', content: `${action === 'friend' ? `Error while killing friendship with ${username}` : `Error while unblocking ${username}`}` });
   }
 
-
   return (
-    <ul className="place-items-stretch grid md:grid grid-cols-2 md:grid-cols-3 gap-10 text-center pt-10 pb-10">
+    <ul className={`place-items-stretch grid md:grid grid-cols-2 md:grid-cols-3 gap-10 text-center pt-10 pb-10 ${type === 'suggested' && "pr-0 md:pr-3"}`}>
       <ResponsiveFade 
         useMediaQueryArg={{ query: "(min-width: 1280px)" }}
         cascade triggerOnce duration={500}
       >
-      {list.map(({ id, username }) => (
+      {list.map(({ id, username, duoquadra_login, wins, losses }) => (
         <li
-          className="text-pink-600 justity-items-center p-5 bg-gray-800/70 border border-pink-600 rounded  hover:border-white hover:bg-pink-700 hover:text-inherit md:transition md:transform md:ease-in md:duration-500 md:hover:-translate-y-2"
+          className="text-pink-600 justity-items-center px-5 pt-2 pb-7 bg-gray-800/70 border border-pink-600 rounded  hover:border-white hover:bg-pink-700 hover:text-inherit md:transition md:transform md:ease-in md:duration-500 md:hover:-translate-y-2"
           key={id}
         >
-          <p className="pb-0 md:pb-5 place-content-evenly text-ellipsis overflow-hidden cursor-pointer font-bold">
-            {username}
-          </p>
-        
+          { type === 'suggested' && 
+          <div className="relative flex items-center justify-center -bottom-4 gap-x-2">
+            <div className="border rounded-full absolute -top-3 md:-top-10 -right-2 md:-right-8 border-pink-600 hover:border-white bg-pink-600 hover:bg-white text-white hover:text-pink-600">
+              <MdCancel
+                className="cursor-pointer text-2xl"
+                onClick={() => setSuggested(suggested.filter(
+                  item => item.id !== id
+                ))}
+              />
+            </div>
+          </div>
+          }
+         
           <div className="relative md:absolute left-0 right-0 flex items-center justify-center -bottom-4 gap-x-2">
           {(type === 'suggested' || type === 'blocked')  &&
             <Tooltip className='font-bold bg-gray-900 text-neutral-200' content="Add as friend">
               <button className="p-2 text-2xl text-gray-900 bg-white rounded-full transition hover:scale-105">
-                <AiOutlineUserAdd onClick={() => {requestFriend(id, username)}} />
+                <AiOutlineUserAdd onClick={() => {requestFriend(id, username, duoquadra_login ? true : false)}} />
               </button>
             </Tooltip>
           }
@@ -152,14 +165,36 @@ const Table: React.FC<{ type: string, list: List[], setList: any, suggested: Lis
 
           <Link href={`/users/${id}`}>
             <a>
-              <div>
+              <div className={`grid grid-cols-2 m-2 space-x-3`}>
+              <div className="grid grid-cols-1 text-center place-items-center text-yellow-500 border-pink-600 bg-inherit">
+                <FaMedal/> {wins}
+              </div>
+              <div className="grid grid-cols-1 text-center place-items-center text-red-500 bg-inherit">
+                <AiOutlineFall className="text-xl font-bold"/> {losses}
+              </div>
+              </div>
                 <img
 		  	        className="justify-content-center"
 	  		        src={`/api/users/${id}/photo`}
   	        		height="100%"
 	  	  	      width="100%"
-              />
-            </div>
+                />
+              {duoquadra_login &&
+              <div className="invisible md:visible absolute top-16 right-5">
+                <div className="flex flex-col items-center p-2">
+                <div className="opacity-70 border border-pink-600 bg-white rounded-full pb-0 pt-2 pr-2 pl-2">
+                  <Image
+                    src="/logo.svg"
+                    width={25}
+                    height={25}
+                    alt="42 Badge"
+                  />
+                </div>
+              </div></div>
+              }
+              <p className="place-content-evenly text-ellipsis overflow-hidden cursor-pointer font-bold">
+                  {username}
+              </p>
             </a>
           </Link>
         </li>
@@ -222,7 +257,7 @@ const FriendsPage: NextPageWithLayout = ({}) => {
   const [friendsLen, setFriendsLen] = useState(0);
   const [friends42Len, setFriends42Len] = useState(0);
   const [blockedLen, setBlockedLen] = useState(0);
-  const [suggestedLen, setSuggestedLen] = useState(0);
+ // const [suggestedLen, setSuggestedLen] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [selected, setSelected] = useState(0);
   const { getUserData } = useContext(authContext) as AuthContextType;
@@ -260,12 +295,10 @@ const FriendsPage: NextPageWithLayout = ({}) => {
       for (var i in data) {
         if (data[i].id !== getUserData().id
           && checkSuggested(blocked, data[i].id)
-          && checkSuggested(friends, data[i].id)) {
+          && checkSuggested(friends, data[i].id))
             suggested = [...suggested, data[i]]
-          }
-        }
+      }
     }
-
     
     setFriends(friends);
     setFriends42(friends42);
@@ -343,19 +376,19 @@ const FriendsPage: NextPageWithLayout = ({}) => {
             items={[
               {
                 label: "Friends",
-                component: <Table type={'friends'} list={friends} setList={setFriends} suggested={suggested} setSuggested={setSuggested} friendsLen={friendsLen} setFriendsLen={setFriendsLen} suggestedLen={suggestedLen} setSuggestedLen={setSuggestedLen} blockedLen={blockedLen} setBlockedLen={setBlockedLen} blockedUsers={blockedUsers} setBlockedUsers={setBlockedUsers} friends={friends} setFriends={setFriends}/>,
+                component: <Table type={'friends'} list={friends} suggested={suggested} setSuggested={setSuggested} friendsLen={friendsLen} setFriendsLen={setFriendsLen} blockedLen={blockedLen} setBlockedLen={setBlockedLen} blockedUsers={blockedUsers} setBlockedUsers={setBlockedUsers} friends={friends} setFriends={setFriends} friends42Len={friends42Len} setFriends42Len={setFriends42Len}/>,
               },
               {
                 label: "Friends @42",
-                component: <Table type={'friends42'} list={friends42} setList={setFriends42} suggested={suggested} setSuggested={setSuggested} friendsLen={friendsLen} setFriendsLen={setFriendsLen} suggestedLen={suggestedLen} setSuggestedLen={setSuggestedLen} blockedLen={blockedLen} setBlockedLen={setBlockedLen} blockedUsers={blockedUsers} setBlockedUsers={setBlockedUsers} friends={friends} setFriends={setFriends}/>,
+                component: <Table type={'friends42'} list={friends42} suggested={suggested} setSuggested={setSuggested} friendsLen={friendsLen} setFriendsLen={setFriendsLen} blockedLen={blockedLen} setBlockedLen={setBlockedLen} blockedUsers={blockedUsers} setBlockedUsers={setBlockedUsers} friends={friends} setFriends={setFriends} friends42Len={friends42Len} setFriends42Len={setFriends42Len}/>,
               },
               {
                 label: "Blocked Users",
-                component: <Table type={'blocked'} list={blockedUsers} setList={setBlockedUsers} suggested={suggested} setSuggested={setSuggested} friendsLen={friendsLen} setFriendsLen={setFriendsLen} suggestedLen={suggestedLen} setSuggestedLen={setSuggestedLen} blockedLen={blockedLen} setBlockedLen={setBlockedLen} blockedUsers={blockedUsers} setBlockedUsers={setBlockedUsers} friends={friends} setFriends={setFriends}/>,
+                component: <Table type={'blocked'} list={blockedUsers} suggested={suggested} setSuggested={setSuggested} friendsLen={friendsLen} setFriendsLen={setFriendsLen} blockedLen={blockedLen} setBlockedLen={setBlockedLen} blockedUsers={blockedUsers} setBlockedUsers={setBlockedUsers} friends={friends} setFriends={setFriends} friends42Len={friends42Len} setFriends42Len={setFriends42Len}/>,
               },
               {
                 label: "Suggested Friends",
-                component: <Table type={'suggested'} list={suggested} setList={setSuggested} suggested={suggested} setSuggested={setSuggested} friendsLen={friendsLen} setFriendsLen={setFriendsLen} suggestedLen={suggestedLen} setSuggestedLen={setSuggestedLen} blockedLen={blockedLen} setBlockedLen={setBlockedLen} blockedUsers={blockedUsers} setBlockedUsers={setBlockedUsers} friends={friends} setFriends={setFriends}/>,
+                component: <Table type={'suggested'} list={suggested} suggested={suggested} setSuggested={setSuggested} friendsLen={friendsLen} setFriendsLen={setFriendsLen} blockedLen={blockedLen} setBlockedLen={setBlockedLen} blockedUsers={blockedUsers} setBlockedUsers={setBlockedUsers} friends={friends} setFriends={setFriends} friends42Len={friends42Len} setFriends42Len={setFriends42Len}/>,
               },
             ]} />
         </div>
