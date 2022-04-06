5de585e9dde1d8df22b5df865576383c4cc80a6a
Mathilde BOIVIN <mboivin@student.42.fr>
2022-04-06 16:55:57 +0200

    Remove unused functions in chat backend

--- src/nestjs-back/src/chat/chat.gateway.ts
+++ src/nestjs-back/src/chat/chat.gateway.ts
@@ -2,11 +2,9 @@ import { Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
-  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
-  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
@@ -30,29 +28,4 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
   handleDisconnect(socket: Socket) {
     this.logger.log(`Client disconnected: ${socket.id}`);
   }
-
-  @SubscribeMessage('messageToServer')
-  async handleMessage(
-    @ConnectedSocket() socket: Socket,
-    @MessageBody() data: string
-  ) {
-    // const author = await this.chatService.getUserFromSocket(socket);
-
-    this.logger.log(`Handle message from Client [${socket.id}]\n${data}`);
-    this.server.emit('messageToClient', data);
-    // this.server.to(data.channel).emit('messageToClient', data);
-  }
-
-  // @SubscribeMessage('joinChannel')
-  // joinChannel(
-  //   @ConnectedSocket() socket: Socket,
-  //   @MessageBody() data: string
-  // ) {
-  //   socket.join(data, (err) => {
-  //     if (err) {
-  //       console.log('error ', err);
-  //     }
-  //   });
-  //   this.logger.log(`Client [${socket.id}] joined channel ${channelName}`);
-  // }
 }
