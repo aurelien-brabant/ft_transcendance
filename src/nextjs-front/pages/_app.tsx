d006899010ee7c4b98c394ffb19d7f1cf56e5efb
abrabant <abrabant@student.42.fr>
2022-04-09 22:40:34 +0200

    Add new auth context and useSession hook to manage auth on the client side

--- src/nextjs-front/pages/_app.tsx
+++ src/nextjs-front/pages/_app.tsx
@@ -1,48 +1,72 @@
-import "../styles/globals.css";
-import { NextPage } from "next";
-import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { ReactElement, ReactNode } from "react";
-import Authenticator, { AuthConfig } from "../components/hoc/withAuth";
-import AuthProvider from "../context/auth/AuthProvider";
-import AlertProvider from "../context/alert/AlertProvider";
-import RelationshipProvider from "../context/relationship/RelationshipProvider";
-import ChatProvider from "../context/chat/ChatProvider";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { NextPage } from 'next';
+import React, { ReactElement, ReactNode, useEffect } from 'react';
+import AuthProvider from '../context/auth/auth-provider';
+import AlertProvider from '../context/alert/AlertProvider';
+import { useRouter } from 'next/router';
+import RelationshipProvider from '../context/relationship/RelationshipProvider';
+import { useSession } from '../hooks/use-session';
+import PageLoadingScreen from '../components/page-loading-screen';
+
+export type PageAuthConfig =
+    | {
+          fallbackUrl?: string;
+      }
+    | boolean;
 
 export type NextPageWithLayout<T = {}> = NextPage<T> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-  isAuthRestricted?: boolean;
-  authConfig?: AuthConfig;
+    getLayout?: (page: ReactElement) => ReactNode;
+    isAuthRestricted?: boolean;
+    authConfig?: PageAuthConfig;
 };
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
+    Component: NextPageWithLayout;
 };
 
-function MyApp({ Component, pageProps, ...rest }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout || ((page) => page);
-  const router = useRouter();
+const AuthenticatedPage: React.FC<{ config: PageAuthConfig }> = ({
+    config,
+    children,
+}) => {
+    const session = useSession({ waitingTime: 1500 });
+    const router = useRouter();
 
-  return (
-    <AlertProvider>
-      <AuthProvider>
-          <RelationshipProvider>
-            <ChatProvider>
-              <Authenticator
-                key={router.asPath}
-                authConfig={
-                  Component.isAuthRestricted
-                    ? Component.authConfig
-                    : { shouldBeAuthenticated: false }
-                }
-              >
-              {getLayout(<Component {...pageProps} />)}
-            </Authenticator>
-          </ChatProvider>
-        </RelationshipProvider>
-      </AuthProvider>
-    </AlertProvider>
-  );
+    console.log(session.state);
+
+    if (session.state !== 'authenticated') {
+        if (session.state === 'unauthenticated') {
+            router.push(
+                (typeof config !== 'boolean' && config.fallbackUrl) || '/signin'
+            );
+        } else if (session.state === 'tfa_required') {
+            router.push('/validate-tfa');
+        }
+
+        return <PageLoadingScreen />;
+    }
+
+    return <>{children}</>;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+    const getLayout = Component.getLayout || ((page) => page);
+
+    return (
+        <AlertProvider>
+            <AuthProvider>
+                {/*<RelationshipProvider>*/}
+                {Component.authConfig ? (
+                    <AuthenticatedPage config={Component.authConfig}>
+                        {getLayout(<Component {...pageProps} />)}
+                    </AuthenticatedPage>
+                ) : (
+                    getLayout(<Component {...pageProps} />)
+                )}
+                {/*</RelationshipProvider>*/}
+            </AuthProvider>
+        </AlertProvider>
+    );
 }
 
 export default MyApp;
