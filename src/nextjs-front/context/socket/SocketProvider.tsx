f1e3f2d76216b789a342db02bccc93e1809b6353
abrabant <abrabant@student.42.fr>
2022-04-21 23:14:06 +0200

    Fix socket listeners on other components

--- src/nextjs-front/context/socket/SocketProvider.tsx
+++ src/nextjs-front/context/socket/SocketProvider.tsx
@@ -4,75 +4,75 @@ import { useSession } from "../../hooks/use-session";
 import socketContext from "./socketContext";
 
 export type ChatUser = {
-	id : string,
-	username: string,
-	socketId: string,
-}
+  id: string;
+  username: string;
+  socketId: string;
+};
 
 const SocketProvider: React.FC = ({ children }) => {
+  const session = useSession();
+  const user = session.user;
+  const [chatRoom, setChatRoom] = useState<ChatUser[]>([]);
+  const [chatRoomLen, setChatRoomLen] = useState(0);
+  const [socket, setSocket] = useState<any>(null);
 
-	const session = useSession();
-	const user = session.user;
-	const [chatRoom, setChatRoom] = useState<ChatUser[]>([]);;
-	const [chatRoomLen, setChatRoomLen] = useState(0);
-	const socket = io('localhost:8080');
+  useEffect((): any => {
+    const socket = io("localhost:8080");
 
-	useEffect((): any => {
+    setSocket(socket);
+    console.log("SOCKET PROVIDER USEEFFECT");
 
-		console.log("SOCKET PROVIDER USEEFFECT");
+    if (!socket || session.state !== "authenticated") return;
 
-		if (!socket || session.state !== 'authenticated')
-			return;
+    const handleChat = () => {
+      socket.on("connect", () => {
+        console.log("[Chat] Client connected");
 
-		const handleChat = () => {
-			socket.on("connect", () => {
-				console.log('[Chat] Client connected');
+        socket.on("connect_error", (err) => {
+          console.log(`connect_error due to ${err.message}`);
+          socket.close();
+        });
 
-				socket.on('connect_error', (err) => {
-					console.log(`connect_error due to ${err.message}`);
-					socket.close();
-				});
+        socket.emit("newUser", {
+          id: user.id,
+          username: user.username,
+        });
 
-				socket.emit('newUser', {
-					id: user.id,
-					username: user.username
-				});
+        socket.on("newDm", () => {
+          console.log("[Chat] newDm -> SOCKET PROVIDER");
+        });
 
-				socket.on('newDm', () => {
-					console.log("[Chat] newDm -> SOCKET PROVIDER");
-				});
+        // socket.on("joinChat", (data: ChatUser[]) => {
+        // 	setChatRoom(data);
+        // });
 
-				// socket.on("joinChat", (data: ChatUser[]) => {
-				// 	setChatRoom(data);
-				// });
+        // socket.on("leaveChat", (data: ChatUser[]) => {
+        // 	setChatRoom(data);
+        // });
 
-				// socket.on("leaveChat", (data: ChatUser[]) => {
-				// 	setChatRoom(data);
-				// });
+        // socket.on('updateChatRoomLen', (len: number) => {
+        // 	setChatRoomLen(len);
+        // });
+      });
+    };
+    handleChat();
 
-				// socket.on('updateChatRoomLen', (len: number) => {
-				// 	setChatRoomLen(len);
-				// });
-			});
-		}
-		handleChat();
+    return () => {
+      socket.disconnect();
+    };
+  }, [user]);
 
-		return () => {
-			socket.disconnect();
-		}
-	}, [user]);
-
-	return (
-		<socketContext.Provider
-			value={{
-				socket,
-				chatRoom,
-				chatRoomLen
-			}}
-		>
-			{children}
-		</socketContext.Provider>
-	);
+  return (
+    <socketContext.Provider
+      value={{
+        socket,
+        chatRoom,
+        chatRoomLen,
+      }}
+    >
+      {children}
+    </socketContext.Provider>
+  );
 };
 
 export default SocketProvider;
