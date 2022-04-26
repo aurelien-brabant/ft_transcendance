e4062030a2bb7b12d08be98295a14758ef07f2de
ouram <mboivin@student.42.fr>
2022-04-26 17:09:00 +0200

    Don't disconnect user

--- src/nestjs-back/src/chat/chat.gateway.ts
+++ src/nestjs-back/src/chat/chat.gateway.ts
@@ -4,7 +4,7 @@ import {
   ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
-  OnGatewayDisconnect,
+  // OnGatewayDisconnect,
   OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
@@ -17,11 +17,10 @@ import { ConnectedUsers, User } from '../games/class/ConnectedUsers';
 @WebSocketGateway(
   {
     cors: true,
-    allowEIO3: true,
     // namespace: '/chat'
   }
 )
-export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection {
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('Chat Gateway');
   private readonly chatUsers: ConnectedUsers = new ConnectedUsers();
@@ -36,14 +35,14 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
     // this.logger.log(`Client connected: ${client.id}`);
   }
 
-  handleDisconnect(client: Socket) {
-    const user = this.chatUsers.getUser(client.id);
+  // async handleDisconnect(@ConnectedSocket() client: Socket) {
+  //   const user = this.chatUsers.getUser(client.id);
 
-    if (user) {
-      this.chatUsers.removeUser(user);
-      // this.logger.log(`Client disconnected: ${user.username}`)
-    }
-  }
+  //   if (user) {
+  //     this.logger.log(`Remove user: [${user.id}][${user.username}]`);
+  //     this.chatUsers.removeUser(user);
+  //   }
+  // }
 
   @SubscribeMessage('newUser')
   handleNewUser(@ConnectedSocket() client: Socket, @MessageBody() data: User) {
@@ -58,6 +57,7 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
       user.setSocketId(client.id);
       user.setUsername(data.username);
     }
+    this.logger.log(`Add user: [${user.id}][${user.username}]`);
     console.log(this.chatUsers);
   }
 
@@ -66,15 +66,16 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
     @ConnectedSocket() client: Socket,
     @MessageBody() data: { content: string, from: number, to: number, channelId: string }
   ) {
-    const user = await this.chatUsers.getUserById(data.from);
-    const message = await this.chatService.saveMessage(data.content, data.from.toString(), data.channelId);
+    const user = this.chatUsers.getUserById(data.from);
+    const recipient = this.chatUsers.getUserById(data.to);
+    const message = await this.chatService.saveMessage(data.content, user.id.toString(), data.channelId);
 
     this.logger.log(`${user.username} sends DM "${data.content}" on channel ${data.channelId}`);
     this.logger.log(`Emitting to ${user.username} -> socket id: [${user.socketId}]`);
     this.server.to(user.socketId).emit('newDm', { message });
     // TODO: If recipient is offline, message will be sent once connected
-    const recipient = this.chatUsers.getUserById(data.to);
     if (recipient) {
+      this.logger.log(`Emitting to ${recipient.username} -> socket id: [${recipient.socketId}]`);
       this.server.to(recipient.socketId).emit('newDm', { message });
     }
   }
@@ -82,13 +83,11 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
   @SubscribeMessage('gmSubmit')
   async handleGmSubmit(
     @ConnectedSocket() client: Socket,
-    @MessageBody() data: { content: string, groupId: string }
+    @MessageBody() data: { content: string, from: number, groupId: string }
   ) {
-    console.log('-----------> gmSubmit');
-    console.log(data);
     const user = this.chatUsers.getUser(client.id);
     // const channel = ;
-    const message = await this.chatService.saveMessage(data.content, user.id.toString(), data.groupId);
+    const message = await this.chatService.saveMessage(data.content, data.from.toString(), data.groupId);
 
     this.logger.log(`${user.username} sends message "${data.content}" on channel ${data.groupId}`);
     this.server.to(client.id).emit('newGm', { message });
