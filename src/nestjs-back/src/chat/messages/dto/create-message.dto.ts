188ef7f78fc2bd23a422bb134d876459518d9352
ouram <mboivin@student.42.fr>
2022-04-27 16:31:57 +0200

    Remove creation date from create DTO

--- src/nestjs-back/src/chat/messages/dto/create-message.dto.ts
+++ src/nestjs-back/src/chat/messages/dto/create-message.dto.ts
@@ -1,25 +1,17 @@
 import {
-  IsDate,
   IsNotEmpty,
-  IsOptional,
   IsString,
-  MaxLength,
-  MinLength
+  MaxLength
 } from 'class-validator';
 import { Transform } from 'class-transformer';
 import { Channel } from "src/chat/channels/entities/channels.entity";
 import { User } from "src/users/entities/users.entity";
 
 export class CreateMessageDto {
-  @IsOptional()
-  @IsDate()
-  readonly createdAt?: Date;
-
   @Transform(({ value }) => value.trim())
   @IsNotEmpty()
   @IsString()
   @MaxLength(640)
-  @MinLength(1)
   readonly content: string;
 
   @IsNotEmpty()
