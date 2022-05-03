fef12361289b616d9ac6d55192ac3d52267cf2ea
ouram <mboivin@student.42.fr>
2022-05-03 17:30:13 +0200

    Remove Direct Message controller

--- src/nestjs-back/src/chat/direct-messages/direct-messages.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('direct-messages')
-export class DirectMessagesController {}
--- src/nestjs-back/src/chat/direct-messages/direct-messages.module.ts
+++ src/nestjs-back/src/chat/direct-messages/direct-messages.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DirectMessage } from './entities/direct-messages';
-import { DirectMessagesController } from './direct-messages.controller';
 import { DirectMessagesService } from './direct-messages.service';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([DirectMessage]), UsersModule],
-  controllers: [DirectMessagesController],
   providers: [DirectMessagesService],
   exports: [DirectMessagesService]
 })
