34a516619d0abf319223b622e7216c9534f42e61
ouram <mboivin@student.42.fr>
2022-05-15 16:53:36 +0200

    Remove unused Achievements routes and add Guards

--- src/nestjs-back/src/achievements/achievements.controller.ts
+++ src/nestjs-back/src/achievements/achievements.controller.ts
@@ -1,38 +1,14 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AchievementsService } from './achievements.service';
-import { CreateAchievementDto } from './dto/create-achievement.dto';
-import { UpdateAchievementDto } from './dto/update-achievement.dto';
+import { JwtAuthGuard } from "../auth/guard/jwt-auth.guard";
 
 @Controller('achievements')
 export class AchievementsController {
     constructor(private readonly achievementsService: AchievementsService) {}
 
+    @UseGuards(JwtAuthGuard)
     @Get()
     findAll() {
         return this.achievementsService.findAll();
-    }
-
-    @Get(':id')
-    async findOne(@Param('id') id: string) {
-        return this.achievementsService.findOne(id).catch((err) => {
-            throw new NotFoundException(err.message);
-        });
-    }
-
-    @Post()
-    create(@Body() createAchievementDto: CreateAchievementDto) {
-        return this.achievementsService.create(createAchievementDto);
-    }
-
-    @Patch(':id')
-    async update(@Param('id') id: string, @Body() updateAchievementDto: UpdateAchievementDto) {
-        return this.achievementsService.update(id, updateAchievementDto).catch((err) => {
-            throw new NotFoundException(err.message);
-        });
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.achievementsService.remove(id);
     }
 }
