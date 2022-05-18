363682d986c401b149bafcddc6df0a5c9a820e75
ouram <mboivin@student.42.fr>
2022-05-19 00:18:02 +0200

    Clean the welcome page

--- src/nextjs-front/pages/welcome.tsx
+++ src/nextjs-front/pages/welcome.tsx
@@ -51,6 +51,8 @@ const Welcome: NextPageWithLayout = () => {
   const [currentStep, setCurrentStep] = useState(0);
   const inputToFocus = useRef<HTMLInputElement>(null);
 
+  let baseObject: FormData;
+
   const [formData, setFormData] = useState<FormData>({
     username: user.username,
     email: user.email,
@@ -58,67 +60,7 @@ const Welcome: NextPageWithLayout = () => {
     pic: user.pic
   });
 
-  let baseObject: FormData;
-
-  const reactivateAccount = () => {
-    backend.request(`/api/users/${user.id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        accountDeactivated: false
-      })
-    });
-  }
-
-  useEffect(() => {
-    if (user.accountDeactivated)
-      reactivateAccount();
-
-    baseObject = {
-      username: user.username,
-      email: user.email,
-      tfa: user.tfa,
-      pic: user.pic
-    }
-  }, [])
-
-  // recompute this only when formData changes
-  const hasPendingChanges = useMemo(
-    () => JSON.stringify(formData) !== JSON.stringify(baseObject),
-    [formData]
-  );
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInvalidInputs({ ...invalidInputs, [e.target.name]: undefined });
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleLogout = async () => {
-		setAlert({type: 'success', content: 'Logged out'});
-		await logout();
-	}
-
-  const deactivateAccount = async () => {
-    const res = await backend.request(`/api/users/${user.id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        accountDeactivated: true
-      })
-    });
-
-    if (res.status === 200) {
-      await handleLogout();
-    }
-  }
-
+  /* Form */
   const editUser = async (formData: FormData) => {
     const res = await backend.request(`/api/users/${user.id}`, {
       method: 'PATCH',
@@ -168,8 +110,131 @@ const Welcome: NextPageWithLayout = () => {
     }
   };
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInvalidInputs({ ...invalidInputs, [e.target.name]: undefined });
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // recompute this only when formData changes
+  const hasPendingChanges = useMemo(
+    () => JSON.stringify(formData) !== JSON.stringify(baseObject),
+    [formData]
+  );
+
+  /* Account deactivation/reactivation */
+  const handleLogout = async () => {
+    await logout();
+  }
+
+  const deactivateAccount = async () => {
+    const res = await backend.request(`/api/users/${user.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        accountDeactivated: true
+      })
+    });
+
+    if (res.status === 200) {
+      await handleLogout();
+    }
+  }
+
+  const reactivateAccount = () => {
+    backend.request(`/api/users/${user.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        accountDeactivated: false
+      })
+    });
+  }
+
+  /* Picture */
+  const UploadPic = () => {
+    const [image, setImage] = useState('');
+
+    const uploadToClient = (event:any) => {
+      if (event.target.files && event.target.files[0]) {
+        const img = event.target.files[0];
+        setImage(img);
+      }
+  };
+
+  const uploadToServer = async () => {
+    const body = new FormData();
+    body.append("image", image);
+
+    const req = await backend.request(`/api/users/${user.id}/uploadAvatar`, {
+      method: "POST",
+      body
+    });
+
+    if (req.ok) {
+      const res = await req.json();
+
+      setPendingPic(false);
+      setAlert({type: 'success', content: 'Avatar uploaded successfully'})
+      router.reload();
+    }
+    else if (req.status === 406)
+      setAlert({type: 'warning', content: 'Only JPG/JPEG/PNG/GIF are accepted'})
+    else if (req.status === 413)
+      setAlert({type: 'warning', content: 'File size too big!'})
+    else
+      setAlert({type: 'error', content: 'Error while uploading!'})
+    };
+
+    return (
+      <ResponsiveSlide
+        useMediaQueryArg={{ query: "(min-width: 1280px)" }}
+        direction="left" duration={200} triggerOnce
+      >
+        <div className="flex justify-center text-pink-600 space-x-5 text-center items-center">
+          <input
+            type="file"
+            name="uploadAvatar"
+            className="border border-pink-600 p-1"
+            onChange={uploadToClient}
+          />
+          <FiUploadCloud onClick={uploadToServer} className="text-3xl hover:animate-pulse"/>
+        </div>
+      </ResponsiveSlide>
+    )
+  }
+
+  const getRandomPic = async () => {
+    setAlert({type: 'info', content: 'New random avatar'})
+
+    const req = await backend.request(`/api/users/${user.id}/randomAvatar`);
+
+    if (req.ok)
+      router.reload();
+    else
+      setAlert({type: 'error', content: 'Error while changin avatar!'})
+  }
+
+  const get42Pic = async () => {
+    setAlert({type: 'info', content: 'Default 42 pic requested'})
+
+    const req = await backend.request(`/api/users/${user.id}/avatar42`);
+    if (req.ok)
+      router.reload();
+    else
+      setAlert({type: 'error', content: 'Error while changin avatar!'})
+  }
+
+  /* TFA */
+
   const activateTfa = async () => {
- 
+
     const req = await backend.request(`/api/users/${user.id}/enableTfa`, {
       method: 'POST',
       headers: {
@@ -194,7 +259,6 @@ const Welcome: NextPageWithLayout = () => {
   }
 
   const deactivateTfa = async () => {
-
     const req = await backend.request(`/api/users/${user.id}`, {
       method: 'PATCH',
       headers: {
@@ -218,6 +282,74 @@ const Welcome: NextPageWithLayout = () => {
     }
   }
 
+  const handleChangeTfa = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const key = e.target.value;
+
+    if (/^[0-9]+$/.test(key)) {
+      e.target.value = tfaCode[currentStep];
+      setCurrentStep(currentStep + 1);
+    }
+    else if (key !== "Escape") {
+      setAlert({ type: 'warning', content: 'Only digit!' });
+      e.target.value = ""
+    }
+  }
+
+  const checkStep = (key: string) => {
+    if (/^[0-9]+$/.test(key))
+      setTfaCode(tfaCode + key);
+
+    if ((currentStep > 0 && (key === "Backspace")) || key === "ArrowLeft") {
+      setTfaCode(tfaCode.substring(0, tfaCode.length - 1));
+      setCurrentStep(currentStep - 1);
+    }
+    else if (key === "Escape") {
+      setPendingQR(false);
+      setTfaCode('');
+      setCurrentStep(0);
+    }
+  }
+
+  const getTfaForm = () => {
+    let content = [];
+
+    for (let i = 0; i < 6; i++) {
+      content.push(
+        <input
+          key={i}
+          ref={currentStep === i ? inputToFocus : null}
+          onChange={handleChangeTfa}
+          className="focus:ring-4 focus:ring-pink-200 focus:ring-offset-pink-200 text-xl md:text-3xl bg-inherit text-pink-600 font-bold text-center border-pink-600 border-2 rounded-lg h-10 md:h-20 w-10 md:w-20"
+          type="text"
+          pattern="[0-9]{1}"
+          onKeyDown={(e) => {checkStep(e.key)}}
+        />
+      );
+    }
+
+    return (
+      <div>
+        <div className="space-x-3 md:space-x-5 my-5 text-center">
+          <h1
+            className="my-10 text-center text-xl text-pink-700 uppercase animate-pulse"
+          >
+            Enter the 6-digit code from your Authenticator App<br/>
+            Press ECHAP to cancel
+          </h1>
+          {content}
+        </div>
+        <div className="flex justify-center p-5">
+          <MdOutlineArrowBackIos
+            className="font-bold text-2xl text-pink-600 hover:animate-bounceBack hover:cursor-pointer"
+            onClick={() => {setPendingQR(false)}}
+          />
+        </div>
+      </div>
+    );
+  };
+
   useEffect(() => {
     if (!tfaCode.length && pendingQR) {
         setAlert({
@@ -229,160 +361,28 @@ const Welcome: NextPageWithLayout = () => {
         activateTfa();
   }, [tfaCode])
 
-  const handleChangeTfa = (e: React.ChangeEvent<HTMLInputElement>) => {
-      e.preventDefault();
-
-      const key = e.target.value;
-
-      if (/^[0-9]+$/.test(key)) {
-          e.target.value = tfaCode[currentStep];
-          setCurrentStep(currentStep + 1);
-      }
-      else if (key !== "Escape") {
-          setAlert({ type: 'warning', content: 'Only digit!' });
-          e.target.value = ""
-      }
-  }
-
-  const checkStep = (key: string) => {
-      if (/^[0-9]+$/.test(key))
-          setTfaCode(tfaCode + key);
-
-      if ((currentStep > 0 && (key === "Backspace")) || key === "ArrowLeft") {
-          setTfaCode(tfaCode.substring(0, tfaCode.length - 1));
-          setCurrentStep(currentStep - 1);
-      }
-      else if (key === "Escape") {
-          setPendingQR(false);
-          setTfaCode('');
-          setCurrentStep(0);
-      }
-  }
-
-  const getTfaForm = () => {
-    
-      let content = [];
-      for (let i = 0; i < 6; i++) {
-        content.push(
-          <input
-              key={i}
-              ref={currentStep === i ? inputToFocus : null}
-              onChange={handleChangeTfa}
-              className="focus:ring-4 focus:ring-pink-200 focus:ring-offset-pink-200 text-xl md:text-3xl bg-inherit text-pink-600 font-bold text-center border-pink-600 border-2 rounded-lg h-10 md:h-20 w-10 md:w-20"
-              type="text"
-              pattern="[0-9]{1}"
-              onKeyDown={(e) => {checkStep(e.key)}}
-          />
-      );
-      }
-
-      return (
-        <div>
-          <div className="space-x-3 md:space-x-5 my-5 text-center">
-            <h1 className="my-10 text-center text-xl text-pink-700 uppercase animate-pulse">
-                    Enter the 6-digit code from your Authenticator App<br/>
-                    Press ECHAP to cancel
-            </h1>
-            {content}
-          </div>
-          <div className="flex justify-center p-5">
-            <MdOutlineArrowBackIos className="font-bold text-2xl text-pink-600 hover:animate-bounceBack hover:cursor-pointer" onClick={() => {setPendingQR(false)}}/>
-          </div>
-        </div>
-      );
-  };
-
-  const UploadPic = () => {
-
-    const [image, setImage] = useState('');
-
-    const uploadToClient = (event:any) => {
-    
-      if (event.target.files && event.target.files[0]) {
-        const img = event.target.files[0];
-        setImage(img);
-      }
-  };
-
-    const uploadToServer = async () => {
-
-      const body = new FormData();
-      body.append("image", image);
-
-      const req = await backend.request(`/api/users/${user.id}/uploadAvatar`, {
-        method: "POST",
-        body
-      });
-
-      if (req.ok) {
-        const res = await req.json();
-
-        setPendingPic(false);
-        setAlert({type: 'success', content: 'Avatar uploaded successfully'})
-        router.reload();
-      }
-      else if (req.status === 406)
-        setAlert({type: 'warning', content: 'Only JPG/JPEG/PNG/GIF are accepted'})
-      else if (req.status === 413)
-        setAlert({type: 'warning', content: 'File size too big!'})
-      else
-        setAlert({type: 'error', content: 'Error while uploading!'})
-    };
-
-    return (
-      <ResponsiveSlide
-        useMediaQueryArg={{ query: "(min-width: 1280px)" }}
-        direction="left" duration={200} triggerOnce
-      >
-        <div className="flex justify-center text-pink-600 space-x-5 text-center items-center">
-          <input
-            type="file"
-            name="uploadAvatar"
-            className="border border-pink-600 p-1"
-            onChange={uploadToClient}
-          />
-          <FiUploadCloud onClick={uploadToServer} className="text-3xl hover:animate-pulse"/>
-        </div>
-      </ResponsiveSlide>
-    )
-  }
-
-  const getRandomPic = async () => {
-
-    setAlert({type: 'info', content: 'New random avatar'})
-
-    const req = await backend.request(`/api/users/${user.id}/randomAvatar`);
-
-    if (req.ok)
-      router.reload();
-    else
-      setAlert({type: 'error', content: 'Error while changin avatar!'})
-  }
-
-  const get42Pic = async () => {
-
-    setAlert({type: 'info', content: 'Default 42 pic requested'})
-
-    const req = await backend.request(`/api/users/${user.id}/avatar42`);
-   
-    if (req.ok)
-      router.reload();
-    else
-      setAlert({type: 'error', content: 'Error while changin avatar!'})
-  }
-
   useEffect(() => {
     inputToFocus.current?.focus();
   }, [currentStep, pendingQR]);
-  
+
+  useEffect(() => {
+    if (user.accountDeactivated) {
+      reactivateAccount();
+    }
+
+    // baseObject = {
+    //   username: user.username,
+    //   email: user.email,
+    //   tfa: user.tfa,
+    //   pic: user.pic
+    // }
+  }, [])
+
   return (
     <div className="text-white" id="main-content">
-      <div 
-        style={{ maxWidth: "800px" }}
-        className="px-2 mx-auto"
-      >
-      
-      {(user.duoquadra_login) ?
+      <div style={{ maxWidth: "800px" }} className="px-2 mx-auto" >
+
+      {(user.duoquadra_login) &&
       <div className="flex flex-col items-center p-2">
         <div className="border border-gray-900 hover:border-pink-600 rounded-full pb-0 pt-2 pr-2 pl-2">
           <Image
@@ -392,24 +392,21 @@ const Welcome: NextPageWithLayout = () => {
             alt="42 logo"
             title="Get intra picture profile"
             className="hover:cursor-pointer"
-            onClick={() => {get42Pic()}}
+            onClick={() => {get42Pic();}}
           />
         </div>
-      </div>
-      :
-      <></>
-      }
+      </div>}
 
-        {!pendingQR ?
-        <Fragment>
+      {!pendingQR ? (
+      <Fragment>
         <div className="flex flex-col items-center gap-y-4">
           <div className="relative w-48 h-48">
-           <img
+            <img
               className="object-cover object-center w-full h-full rounded drop-shadow-md"
               src={`/api/users/${user.id}/photo`}
-            /> 
+            />
 
-            {pendingPic ?
+            {pendingPic ? (
             <div className="absolute p-2 bg-white border-2 border-gray-900 rounded-full -top-4 -right-4">
               <div className="absolute left-0 right-0 flex items-center justify-center -bottom-4 gap-x-2">
                 <Tooltip className="font-bold bg-gray-900 text-neutral-200" content="Cancel">
@@ -422,7 +419,7 @@ const Welcome: NextPageWithLayout = () => {
                 </Tooltip>
               </div>
             </div>
-            :
+            ) : (
             <div>
               <div className="absolute flex -top-4 -left-4">
                 <Tooltip className="font-bold bg-gray-900 text-neutral-200" content="Random avatar">
@@ -445,12 +442,11 @@ const Welcome: NextPageWithLayout = () => {
                 </Tooltip>
               </div>
             </div>
-            }
+            )}
           </div>
-
-          { pendingPic ?
+          {pendingPic ? (
           <UploadPic />
-          :
+          ) : (
           <div className="text-center">
             <h2 className="text-xl font-bold text-pink-600">
               {user.username}
@@ -460,17 +456,17 @@ const Welcome: NextPageWithLayout = () => {
                 See public profile
               </a>
             </Link>
-          </div>
-          }
+          </div>)}
         </div>
 
         <div className="flex flex-col py-12 gap-y-10">
-            <h1 className="text-2xl">
-              Edit profile
-            </h1>
-          
+          <h1 className="text-2xl">
+            Edit profile
+          </h1>
+
           {/* Inputs */}
           <form onSubmit={handleSubmit} className="flex flex-col gap-y-6">
+
             <div className={inputGroupClassName}>
               <label htmlFor="username" className={labelClassName}>
                 Username
@@ -484,7 +480,6 @@ const Welcome: NextPageWithLayout = () => {
                   className={inputClassName}
                 />
               </InputErrorProvider>
-              <small></small>
             </div>
 
             <div className={inputGroupClassName}>
@@ -513,14 +508,13 @@ const Welcome: NextPageWithLayout = () => {
                   if (tfaStatus === 'disabled') {
                     setAlert({ type: 'info', content: 'Scan the QR code to get the 6-digits code given by your authenticator app'})
                     setPendingQR(true);
-                  }
-                  else {
+                  } else {
                     if (confirm("Deactivated 2FA with Authenticator App?\nYou can reactivate it when you want.\n\nClick OK to proceed.") == true)
                       deactivateTfa();
-                    }
+                  }
                 }}
               >
-                {(tfaStatus === 'enabled') ? "DEACTIVATE 2FA" : "ACTIVATE 2FA"}
+              {(tfaStatus === 'enabled') ? "DEACTIVATE 2FA" : "ACTIVATE 2FA"}
               </button>
               <small>
                 Confirm each connection to your account using an Authenticator App
@@ -528,7 +522,6 @@ const Welcome: NextPageWithLayout = () => {
             </div>
 
             {/* actions */}
-
             <div className="flex flex-col justify-between py-5 gap-y-4 md:gap-y-0 md:flex-row">
               <button
                 type="submit"
@@ -551,20 +544,22 @@ const Welcome: NextPageWithLayout = () => {
                 Deactivate account
               </button>
             </div>
+
           </form>
+
         </div>
-        </Fragment>
-        :
-        <div className="flex flex-col items-center gap-y-4">
-          <div className="relative w-48 h-48">
-            <img
-              className="object-cover object-center w-full h-full rounded drop-shadow-md"
-              src={`/api/users/${user.id}/generateTfa`}
-            />
-          </div>
-          {getTfaForm()}
+      </Fragment>
+      ) : (
+      <div className="flex flex-col items-center gap-y-4">
+        <div className="relative w-48 h-48">
+          <img
+            className="object-cover object-center w-full h-full rounded drop-shadow-md"
+            src={`/api/users/${user.id}/generateTfa`}
+          />
         </div>
-        }
+        {getTfaForm()}
+      </div>
+      )}
       </div>
     </div>
   );
