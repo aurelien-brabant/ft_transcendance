13ef59a11ea509696face65a1f1755f2df921a5e
ouram <mboivin@student.42.fr>
2022-05-20 18:58:14 +0200

    Clean file

--- src/nextjs-front/components/Canvas.tsx
+++ src/nextjs-front/components/Canvas.tsx
@@ -41,28 +41,33 @@ const Canvas: React.FC<{socketProps: Socket, roomProps: any}> = ({socketProps, r
 	const drawGame = (canvas: HTMLCanvasElement, draw: Draw, room: IRoom): void => {
 		draw.clear();
 
-		if (room.mode === GameMode.TIMER)
+		if (room.mode === GameMode.TIMER) {
 			draw.drawTimer(room);
+		}
 		draw.drawNet();
 		draw.drawScore(room.playerOne, room.playerTwo);
 		draw.drawPaddle(room.playerOne);
 		draw.drawPaddle(room.playerTwo);
-		if (room.gameState !== GameState.PLAYERONESCORED && room.gameState !== GameState.PLAYERTWOSCORED
-			&& room.gameState !== GameState.PLAYERONEWIN && room.gameState !== GameState.PLAYERTWOWIN)
+
+		if (room.gameState !== GameState.PLAYERONESCORED
+			&& room.gameState !== GameState.PLAYERTWOSCORED
+			&& room.gameState !== GameState.PLAYERONEWIN
+			&& room.gameState !== GameState.PLAYERTWOWIN) {
 			draw.drawBall(room.ball);
+		}
 		draw.animateNeon(canvas);
 	}
 
 	useEffect(() => {
 		const canvas = canvasRef.current;
-		if (!canvas)
+		if (!canvas) {
 			return ;
-		// const context = canvas.getContext('2d');
+		}
+
 		let animationFrameId: number;
 
 		canvas.width = canvasWidth;
 		canvas.height = canvasHeight;
-
 
 		const draw: Draw = new Draw(canvas);
 
@@ -89,22 +94,25 @@ const Canvas: React.FC<{socketProps: Socket, roomProps: any}> = ({socketProps, r
 			draw.drawGoalParticle(room.ball);
 			draw.drawRectangle(0, 0, canvasWidth, canvasHeight, "rgba(0, 0, 0, 0.4)");
 			seconds += secondElapsed;
-			if (seconds >= 1)
-			{
-				if (display < 3)
+
+			if (seconds >= 1) {
+				if (display < 3) {
 					display++;
+				}
 				seconds -= 1;
 			}
+
 			if (display >= 1) {
-				if (room.gameState === GameState.PLAYERONEWIN)
+				if (room.gameState === GameState.PLAYERONEWIN) {
 					draw.drawCenteredText((room.playerOne.user.username + " Won !!!"), canvasWidth/2, ((canvasHeight/2) - (canvasHeight/10)), 45, 'white');
-				else 
+				} else {
 					draw.drawCenteredText((room.playerTwo.user.username + " Won !!!"), canvasWidth/2, ((canvasHeight/2) - (canvasHeight/10)), 45, 'white');
+				}
 			}
-			if (display >= 2)
-			{
+			if (display >= 2) {
 				draw.drawCenteredText("Match end", canvasWidth/2, ((canvasHeight/2)), 45, 'white');
 			}
+
 			setGameEnded(true);
 		}
 
@@ -112,15 +120,17 @@ const Canvas: React.FC<{socketProps: Socket, roomProps: any}> = ({socketProps, r
 			if (room.gameState !== GameState.PLAYERONEWIN && room.gameState !== GameState.PLAYERTWOWIN && isAplayer) {
 				socket.emit("requestUpdate", roomId);
 			}
+
 			secondElapsed = (timestamp - oldTimestamp) / 1000;
 			oldTimestamp = timestamp;
-
 			drawGame(canvas, draw, room);
+
 			if (room.gameState === GameState.STARTING) {
 				let count: number = (Date.now() - room.timestampStart) / 1000;
 				draw.drawRectangle(0, 0, canvasWidth, canvasHeight, "rgba(0, 0, 0, 0.5)");
 				draw.drawCountDown(countDown[Math.floor(count)]);
 			}
+
 			if (room.gameState === GameState.PLAYING) {
 				draw.resetParticles();
 			} else if (room.gameState === GameState.PAUSED) {
@@ -153,42 +163,40 @@ const Canvas: React.FC<{socketProps: Socket, roomProps: any}> = ({socketProps, r
 	}, []);
 
 	return (
-		<>		
-		{
-			room &&
-				<div className="flex flex-col items-center gap-y-10">	
-					<canvas ref={canvasRef} className={styles.canvas} ></canvas>
-						<div className="flex justify-between w-full">
-							<div className="flex gap-x-5 items-center">
-								<img
-									className="rounded-full sm:block"
-									height="45px"
-									width="45px"
-									src={`/api/users/${room.playerOne.user.id}/photo`}
-									alt="user's avatar"
-								/>
-								<div>{room.playerOne.user.username}</div>
-							</div>
-							<div className="flex gap-x-5 items-center">
-								<div>{room.playerTwo.user.username}</div>
-								<img
-									className="rounded-full sm:block"
-									height="45px"
-									width="45px"
-									src={`/api/users/${room.playerTwo.user.id}/photo`}
-									alt="user's avatar"
-								/>
-							</div>
+		<>
+		{room &&
+			<div className="flex flex-col items-center gap-y-10">	
+				<canvas ref={canvasRef} className={styles.canvas} ></canvas>
+					<div className="flex justify-between w-full">
+						<div className="flex gap-x-5 items-center">
+							<img
+								className="rounded-full sm:block"
+								height="45px"
+								width="45px"
+								src={`/api/users/${room.playerOne.user.id}/photo`}
+								alt="user's avatar"
+							/>
+							<div>{room.playerOne.user.username}</div>
 						</div>
-						{
-							(gameEnded || !isAplayer) &&
-							<button onClick={leaveRoom} className="px-6 py-2 text-xl uppercase bg-pink-600 drop-shadow-md text-bold text-neutral-200">Leave Room</button>
-						}
-				</div>
+						<div className="flex gap-x-5 items-center">
+							<div>{room.playerTwo.user.username}</div>
+							<img
+								className="rounded-full sm:block"
+								height="45px"
+								width="45px"
+								src={`/api/users/${room.playerTwo.user.id}/photo`}
+								alt="user's avatar"
+							/>
+						</div>
+					</div>
+					{
+						(gameEnded || !isAplayer) &&
+						<button onClick={leaveRoom} className="px-6 py-2 text-xl uppercase bg-pink-600 drop-shadow-md text-bold text-neutral-200">Leave Room</button>
+					}
+			</div>
 		}
 		</>
 	);
 };
-
 
 export default Canvas;
